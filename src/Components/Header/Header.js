5f0ec2513593583757441c6025e37491e7cb8030
pranav02082003 <luckypranav47@gmail.com>
2024-09-16 08:00:00 +0530

    responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export default function Header() {
     return (
         <div>
             <nav className='d-flex justify-content-end nav'>
                 <ul className='ul-div'>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to={'/'}><li className='list'>Home</li></Link>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to={"/Education"}><li className='list'>Education</li></Link>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to={'/Project'}><li className='list'>Projects</li></Link>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to={'/Contact'}><li className='list'>Contact</li></Link>
+                    <Link className="nav-link" style={{ color: 'inherit', textDecoration: 'inherit' }} to={'/'}><li className='list'>Home</li></Link>
+                    <Link className="nav-link" style={{ color: 'inherit', textDecoration: 'inherit' }} to={"/Education"}><li className='list'>Education</li></Link>
+                    <Link className="nav-link" style={{ color: 'inherit', textDecoration: 'inherit' }} to={'/Project'}><li className='list'>Projects</li></Link>
+                    <Link className="nav-link" style={{ color: 'inherit', textDecoration: 'inherit' }} to={'/Contact'}><li className='list'>Contact</li></Link>
                 </ul>
             </nav>
         </div>
